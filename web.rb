e05e8141f40b783029440beb0dbb92083f510e18
shinderuman <shinderuman@gmail.com>
2012-05-30 00:58:16 +0900

    use net/http

--- web.rb
+++ web.rb
@@ -1,12 +1,17 @@
 # encoding: utf-8
 require 'rubygems'
 require 'nokogiri'
-require 'open-uri'
+require 'net/http'
 
 #url = "http://www58.tok2.com/~Asmodeus/"
 url = "https://www.google.co.jp"
-html = Nokogiri::HTML(open(url), nil, 'UTF-8')
+Net::HTTP.version_1_2
+html = nil
+Net::HTTP.start('www58.tok2.com', 80) {|http|
+ html = Nokogiri::HTML(http.get('/home/asmodeus/', '').body, nil, 'UTF-8')
+}
 
 html.search('img').each do |entry|
   puts entry.search('@src').text ; "\n"
 end
+
